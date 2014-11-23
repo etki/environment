20d55d4b5322241513b747688e35051002562d92
Etki <etki@etki.name>
2014-11-23 07:02:57 +0300

    Missing exception class

---
+++ src/Exception/NotImplementedException.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Etki\Environment\Exception;
+
+use LogicException;
+
+/**
+ * Whatever.
+ *
+ * @version 0.1.0
+ * @since   0.1.0
+ * @package Etki\Environment\Exception
+ * @author  Etki <etki@etki.name>
+ */
+class NotImplementedException extends LogicException
+{
+
+}
